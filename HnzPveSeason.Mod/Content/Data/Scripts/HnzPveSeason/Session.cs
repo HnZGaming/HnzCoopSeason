13782e5a971556c7ae6502acc1ced515fd2f7878
Ryoichiro Oka <ryoka0418@gmail.com>
2025-01-28 00:18:34 +0900

    feat: init mod

---
+++ HnzPveSeason.Mod/Content/Data/Scripts/HnzPveSeason/Session.cs
@@ -0,0 +1,17 @@
+﻿using VRage.Game.Components;
+using VRage.Utils;
+
+namespace HnzPveSeason
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
+    // ReSharper disable once UnusedType.Global
+    public sealed class Session : MySessionComponentBase
+    {
+        public override void LoadData()
+        {
+            base.LoadData();
+
+            MyLog.Default.Info("[HnzPveSeason] session loaded");
+        }
+    }
+}

92f69d12572242395fb28c76eb9f8bb8a00bb676
Ryoichiro Oka <ryoka0418@gmail.com>
2025-03-09 23:01:12 +0900

    feat: fill techs on discovery

--- HnzCoopSeason.Mod/Content/Data/Scripts/HnzCoopSeason/PoiMerchant.cs
+++ HnzCoopSeason.Mod/Content/Data/Scripts/HnzCoopSeason/PoiMerchant.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using HnzCoopSeason.Utils;
 using Sandbox.Common.ObjectBuilders;
-using Sandbox.Definitions;
 using Sandbox.Game.Entities;
 using Sandbox.ModAPI;
 using VRage.Game;
@@ -11,7 +10,6 @@ using VRage.Game.ModAPI;
 using VRage.Game.ObjectBuilders.Definitions;
 using VRage.Library.Utils;
 using VRage.ModAPI;
-using VRage.ObjectBuilders;
 using VRage.Serialization;
 using VRage.Utils;
 using VRageMath;
@@ -49,6 +47,7 @@ namespace HnzCoopSeason
             LoadFromSandbox();
             _economyInterval.Initialize();
 
+            // load existing grid
             var myGrids = grids.Where(g => IsMyGrid(g)).ToArray();
             for (var i = 0; i < myGrids.Length; i++)
             {
@@ -176,7 +175,7 @@ namespace HnzCoopSeason
             _grid.UpdateStorageValue(StorageKey, _poiId);
             _spawnState = SpawnState.Success;
 
-            UpdateStore();
+            UpdateStore(true);
             SetUpSafezone();
             SaveToSandbox();
 
@@ -250,11 +249,11 @@ namespace HnzCoopSeason
             MyLog.Default.Info($"[HnzCoopSeason] poi merchant {_poiId} safezone removed");
         }
 
-        public void UpdateStore()
+        public void UpdateStore(bool fill = false)
         {
             if (_grid == null) return;
 
-            MyLog.Default.Info($"[HnzCoopSeason] poi merchant {_poiId} update store items");
+            MyLog.Default.Info($"[HnzCoopSeason] poi merchant {_poiId} update store items; fill: {(fill ? 'o' : 'x')}");
 
             var storeBlocks = _grid.GetFatBlocks<IMyStoreBlock>().Where(b => IsStoreBlock(b)).ToArray();
             if (storeBlocks.Length != 1)
@@ -289,16 +288,17 @@ namespace HnzCoopSeason
             storeBlock.ClearItems();
 
             var allItems = new Dictionary<MyDefinitionId, int>();
-            foreach (var c in SessionConfig.Instance.StoreItems)
+            foreach (var kvp in GetStoreItemConfigs())
             {
-                MyDefinitionId id;
-                if (!MyDefinitionId.TryParse($"{c.Type}/{c.Subtype}", out id))
-                {
-                    MyLog.Default.Error($"[HnzCoopSeason] invalid store item config: {c}");
-                    continue;
-                }
+                var id = kvp.Key;
+                var c = kvp.Value;
 
                 var existingAmount = existingOffers.GetValueOrDefault(id, 0);
+                if (fill)
+                {
+                    existingAmount += c.MaxAmount;
+                }
+
                 var fillAmount = (int)MathHelper.Lerp(c.MinAmountPerUpdate, c.MaxAmountPerUpdate, MyRandom.Instance.NextDouble());
                 var amount = Math.Min(existingAmount + fillAmount, c.MaxAmount);
                 var item = storeBlock.CreateStoreItem(id, amount, c.PricePerUnit, StoreItemTypes.Offer);
@@ -355,6 +355,31 @@ namespace HnzCoopSeason
         static bool IsCargoBlock(IMyCargoContainer block)
         {
             return block.CustomName.Contains("Cargo");
+        }
+
+        static Dictionary<MyDefinitionId, StoreItemConfig> GetStoreItemConfigs()
+        {
+            var items = SessionConfig.Instance.StoreItems;
+            var results = new Dictionary<MyDefinitionId, StoreItemConfig>();
+            foreach (var c in items)
+            {
+                MyDefinitionId id;
+                if (!MyDefinitionId.TryParse($"{c.Type}/{c.Subtype}", out id))
+                {
+                    MyLog.Default.Error($"[HnzCoopSeason] invalid store item config: {c}");
+                    continue;
+                }
+
+                if (results.ContainsKey(id))
+                {
+                    MyLog.Default.Error($"[HnzCoopSeason] duplicate store item config: {c}");
+                    continue;
+                }
+
+                results.Add(id, c);
+            }
+
+            return results;
         }
 
         public override string ToString()

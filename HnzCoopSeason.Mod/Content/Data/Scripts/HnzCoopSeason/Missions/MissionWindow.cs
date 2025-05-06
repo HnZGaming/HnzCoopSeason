74088013f560c0a908573c8cb8c6b7b35369f138
Ryoichiro Oka <ryoka0418@gmail.com>
2025-05-07 02:16:42 +0900

    feat: mock list

--- HnzCoopSeason.Mod/Content/Data/Scripts/HnzCoopSeason/Missions/MissionWindow.cs
+++ HnzCoopSeason.Mod/Content/Data/Scripts/HnzCoopSeason/Missions/MissionWindow.cs
@@ -14,7 +14,11 @@ namespace HnzCoopSeason.Missions
     public class MissionWindow : WindowBase
     {
         readonly IBindGroup _keyBinds;
-        MissionListElement _missionElement;
+        readonly ScrollBox<ScrollBoxEntry<MissionListElement>, MissionListElement> _missionList;
+        readonly HudChain _detailPane;
+        readonly Label _titleLabel;
+        readonly Label _descriptionLabel;
+        readonly LabelBoxButton _submitButton;
 
         public static MissionWindow Instance { get; private set; }
 
@@ -46,8 +50,8 @@ namespace HnzCoopSeason.Missions
         MissionWindow(HudParentBase parent = null) : base(parent)
         {
             header.Format = new GlyphFormat(GlyphFormat.Blueish.Color, TextAlignment.Center, 1.08f);
-            header.Height = 30f;
-            body.Size = new Vector2(500f, 200f);
+            header.Height = 30;
+            body.Size = new Vector2(500, 270);
 
             _keyBinds = BindManager.GetOrCreateGroup("CoopContractBinds");
             _keyBinds.RegisterBinds(new BindGroupInitializer
@@ -57,7 +61,51 @@ namespace HnzCoopSeason.Missions
 
             _keyBinds[0].NewPressed += ToggleWindow;
 
-            _missionElement = new MissionListElement(bodyBg);
+            _missionList = new ScrollBox<ScrollBoxEntry<MissionListElement>, MissionListElement>(true, bodyBg)
+            {
+                SizingMode = HudChainSizingModes.FitMembersOffAxis,
+                ParentAlignment = ParentAlignments.Top | ParentAlignments.Inner | ParentAlignments.Left,
+                Offset = Vector2.Zero,
+                Size = new Vector2(150, 270),
+                Padding = new Vector2(6, 6),
+                Spacing = 12,
+            };
+
+            _detailPane = new HudChain(true, bodyBg)
+            {
+                SizingMode = HudChainSizingModes.FitMembersOffAxis,
+                ParentAlignment = ParentAlignments.Top | ParentAlignments.Inner | ParentAlignments.Right,
+                Offset = Vector2.Zero,
+                Size = new Vector2(270, 270),
+                Padding = new Vector2(6, 6),
+                Spacing = 12,
+            };
+
+            _titleLabel = new Label
+            {
+                ParentAlignment = ParentAlignments.Center | ParentAlignments.Inner | ParentAlignments.Left,
+                Size = new Vector2(250, 20),
+                Text = "Acquisition",
+            };
+
+            _descriptionLabel = new Label
+            {
+                ParentAlignment = ParentAlignments.Center | ParentAlignments.Inner | ParentAlignments.Left,
+                Size = new Vector2(250, 60),
+                Text = "Acquisition",
+            };
+
+            _submitButton = new LabelBoxButton
+            {
+                ParentAlignment = ParentAlignments.Center | ParentAlignments.Inner | ParentAlignments.Left,
+                Size = new Vector2(200, 20),
+                Text = "Submit",
+            };
+
+            _detailPane.Add(_titleLabel);
+            _detailPane.Add(_descriptionLabel);
+            _detailPane.Add(_submitButton);
+            _detailPane.Visible = false;
         }
 
         public void Unload()
@@ -81,7 +129,22 @@ namespace HnzCoopSeason.Missions
 
         void OnMissionsUpdated(Mission[] missions)
         {
-            _missionElement.SetMission(missions[0]);
+            _missionList.Clear();
+            foreach (var mission in missions)
+            {
+                var element = new MissionListElement();
+                element.SetMission(mission);
+                _missionList.Add(element);
+            }
+        }
+
+        public void OnMissionListElementClicked(Mission mission)
+        {
+            MyLog.Default.Info($"[HnzCoopSeason] mission selected: {mission.Title}");
+
+            _detailPane.Visible = true;
+            _titleLabel.Text = mission.Title;
+            _descriptionLabel.Text = mission.Description;
         }
     }
 }
